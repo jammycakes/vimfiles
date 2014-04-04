3d8925a49294f05ec1a8e6fb7ca3bcdb7ca92846
James McKay <mckayj@parliament.uk>
2014-04-04 14:34:48 +0100

    Add bundles vim-indent-guides and vim-less

--- install-bundles.bat
+++ install-bundles.bat
@@ -1,3 +1,6 @@
 mkdir bundle
 git clone https://github.com/scrooloose/nerdtree.git bundle/nerdtree
 git clone https://github.com/PProvost/vim-ps1.git bundle/vim-ps1
+git clone https://github.com/nathanaelkane/vim-indent-guides.git bundle/vim-indent-guides
+git clone https://github.com/groenewege/vim-less.git bundle/vim-less
+
