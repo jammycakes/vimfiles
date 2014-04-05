f5d1a1ca346a8f5d67555e2367f314b88ba82eb5
James McKay <code@jamesmckay.net>
2014-04-05 09:07:13 +0100

    Remove install-bundles.bat, it's no longer needed and useless on unix

--- install-bundles.bat
+++
@@ -1,6 +0,0 @@
-mkdir bundle
-git clone https://github.com/scrooloose/nerdtree.git bundle/nerdtree
-git clone https://github.com/PProvost/vim-ps1.git bundle/vim-ps1
-git clone https://github.com/nathanaelkane/vim-indent-guides.git bundle/vim-indent-guides
-git clone https://github.com/groenewege/vim-less.git bundle/vim-less
-git clone https://github.com/pangloss/vim-javascript.git bundle/vim-javascript
